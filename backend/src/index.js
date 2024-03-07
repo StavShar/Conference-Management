0141c9a57332d18f4d9bcd47290163faad0e0f5e
StavShar <stavshar21@gmail.com>
2024-03-07 02:02:28 +0200

    connect MongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,38 @@
-const express = require('express')
-
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 const port = process.env.PORT;
+const dbURL = process.env.DATABASE_URL;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+async function connecttoDB() {
+    try {
+        console.log('Trying to connect to DB');
+        mongoose.connect(dbURL);
+    } catch (error) {
+        console.log('Error connecting to DB');
+    }
+
+    const db = mongoose.connection
+
+    db.on('error', error => { console.error('Failed to connect to MongoDB: ' + error) });
+    db.once('open', () => { console.log('Connected to MongoDB.') });
+
+}
+connecttoDB();
+
 
 app.get('/', (req, res) => {
     res.send('SERVER STARTED!')
 })
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
+    console.log(`server started on port: ${port}`)
 })
