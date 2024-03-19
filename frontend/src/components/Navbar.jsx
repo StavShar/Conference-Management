0c2fcb26e142b39179fe2ce9c8ba67d3be437435
KaTziRadar <adarkatzir@gmail.com>
2024-03-19 15:58:46 +0200

    navbar update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,28 +1,22 @@
 import {React} from 'react';
-
+import './pages/styles/Navbar.css'
 function Navbar() {
     
     return (
-        <nav className='navbar'>
-            <a href='/' className='site-title'>Home</a>
-            <ul>
-                { (
-                    <>
-                        <li>
-                            <a href='/login'>Login</a>
-                        </li>
-                        <li>
-                            <a href='/signup'>SignUp</a>
-                        </li>
-                        <li>
-                            <a href='/myconferences'>MyConference</a>
-                        </li>
-                        
-                    </>
-                )}
-
-            </ul>
-        </nav>
+        <nav className="navbar">
+      <a href="/" className="navbar-brand">Home</a>
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a href="/login" className="nav-link">Login</a>
+        </li>
+        <li className="nav-item">
+          <a href="/signup" className="nav-link">SignUp</a>
+        </li>
+        <li className="nav-item">
+          <a href="/myconference" className="nav-link nav-link-special">MyConference</a>
+        </li>
+      </ul>
+    </nav>
     );
 }
 export default Navbar;
--- frontend/src/components/pages/Login.jsx
+++ frontend/src/components/pages/Login.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Login() {
   return (
     <div>
-      <h2>Log In Page</h2>
+      <h2>Login page</h2>
       {/* Your log in form can go here */}
     </div>
   );
